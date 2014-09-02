e95a0763a29d106d27f08b4cfeb6becc126efff5
Claire <jfecroft@ubuntu.(none)>
2014-09-01 20:43:13 -0600

    minor change to make answer the actual problem as set.

--- prob9.py
+++ prob9.py
@@ -1,7 +1,7 @@
 
 NumMax =1000 
 
-for a in xrange(NumMax):
- for b in xrange(NumMax-a+1):
+for a in xrange(1,NumMax):
+ for b in xrange(a,NumMax-a+1):
   c = NumMax -a -b
   if a**2+b**2 == c**2:print a*b*c
