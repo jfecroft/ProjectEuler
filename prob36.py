aabecfea8be5bc603d41964fbbe941ee9629c712
CROFT James <jacr7386@jiliac.colorado.edu>
2014-09-03 18:52:34 -0600

    simple improvement check one base, then see if palindrome in second base save checking all in both bases

--- prob36.py
+++ prob36.py
@@ -3,8 +3,15 @@ from Palindrome import IsPalindrome
 
 NumMax = 1000000
 Sum = 0
+PalindromesInBase2=[]
+#check to see if palindrome in base10
 for Num in xrange(NumMax):
- if IsPalindrome(str(Num)) and IsPalindrome(binary_repr(Num)):
+ if IsPalindrome(str(Num)):
+  PalindromesInBase2.append(Num)
+
+#if palindrome in base 2 see if also palindrome in base2
+for Num in  PalindromesInBase2:
+ if IsPalindrome(binary_repr(Num)):
   Sum += Num
 
 print Sum
