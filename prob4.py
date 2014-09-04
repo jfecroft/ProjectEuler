4be19b6c7995940a1361a6bbd8aa2c3661f1fb72
James Croft <jfecroft@gmail.com>
2014-09-03 18:24:01 -0600

    opimized prob4 to use deque. useful for later larger problems.

--- prob4.py
+++ prob4.py
@@ -1,10 +1,17 @@
 import itertools
-
+from collections import deque
 
 def IsPalindrome(String):
- ReversedString = String[::-1]
- if String == ReversedString: return True
- return False
+ character_deque = deque()
+ #put all chracters in deque then check one by one
+ for i in String: character_deque.appendleft(i)
+
+ Equal = True
+ while Equal and len(character_deque) > 1:
+  rightcharacter = character_deque.pop()
+  leftcharacter  = character_deque.popleft()
+  if rightcharacter != leftcharacter: Equal = False #if any not equal then not a palidrome
+ return Equal
 
 NumMax = 999
 Highest = 0
