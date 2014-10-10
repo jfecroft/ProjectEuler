dfba1ce35ce45f923affd92bc98300a6bab6c4e2
James Croft <j.f.e.croft@gmail.com>
2014-10-10 16:59:26 -0600

    solved probelm 21 and modifed some modules. added a setoperations module which can compute the power set

--- PrimeFactorisation.py
+++ PrimeFactorisation.py
@@ -1,6 +1,21 @@
+from SetOperations import powerset
+import numpy as np
+
+def ProperDivisors(n):
+ PF = PrimeFactors(n)
+ PD = list(powerset(PF))
+ PD = [t for t in PD if t != ()]  #remove the empty set
+ for i,j in enumerate(PD):
+  PD[i] = np.prod(np.array(j))
+ PD = set(PD)
+ PD.remove(n)
+ return PD
+
+
 def PrimeFactors(n,PrimesList=None):
   """returns the prime factorisation of n"""
   if PrimesList == None: PrimesList = []
+  if n ==1: return [] #account for empty set for 1
   i=2
   while (n%i != 0 and i < n):
     i += 1
@@ -26,3 +41,4 @@ def PrimesLessThanN(NumMax):
  for i in range(2,len(Primes)):
   if Primes[i] == True: PrimesList.append(i)
  return PrimesList
+
---
+++ prob21.py
@@ -0,0 +1,18 @@
+from PrimeFactorisation import ProperDivisors
+import numpy as np
+
+def d(n):
+ PD = np.array(list(ProperDivisors(n)))
+ return np.sum(PD)
+
+low, high = 2, 10000  #start at 2 to avoid empty set for proper divisors for 1
+sum_divisors = [d(n) for n in xrange(low,high+1)] #get d(n)  for n
+
+pairs=[]
+for i in range(high-low+1):
+ ind = sum_divisors[i] #index to compare to
+ if i + low < ind and low <= ind and ind <= high and sum_divisors[ind - low] == i + low:
+  pairs.append([i+low,ind])
+
+
+print np.sum(np.array(pairs))
