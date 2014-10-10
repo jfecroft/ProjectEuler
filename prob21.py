4278a7c5f7edd4955459798811af91e3f836f734
James Croft <j.f.e.croft@gmail.com>
2014-10-10 17:03:25 -0600

    removed a redundent if statement.

--- prob21.py
+++ prob21.py
@@ -11,7 +11,7 @@ sum_divisors = [d(n) for n in xrange(low,high+1)] #get d(n)  for n
 pairs=[]
 for i in range(high-low+1):
  ind = sum_divisors[i] #index to compare to
- if i + low < ind and low <= ind and ind <= high and sum_divisors[ind - low] == i + low:
+ if i + low < ind and ind <= high and sum_divisors[ind - low] == i + low:
   pairs.append([i+low,ind])
 
 
