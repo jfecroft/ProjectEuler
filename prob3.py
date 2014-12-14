fe0dea1402a181b2c6230dbd9817620152e667d4
James Croft <jfecroft@Jamess-MacBook-Air.local>
2014-12-14 21:18:30 +0000

    Updated prob3 to adhere to PEP8.

--- prob3.py
+++ prob3.py
@@ -1,15 +1,13 @@
-from math import sqrt
-Prime = 42000
-Prime = 600851475143
+prime = 600851475143
 
+def largest_prime_factor(n):
+    """ Return the largest prime factor of n."""
+    i = 2
+    while (n%i != 0 and i < n):
+        i += 1
+    if (i < n):
+        return largest_prime_factor(n/i)
+    else:
+        return n
 
-def prime(n):
-  i=2
-  while (n%i != 0 and i < n):
-    i += 1
-  if (i < n):
-    return prime(n/i)
-  else:
-    print n
-
-prime(Prime)
+print largest_prime_factor(prime)
