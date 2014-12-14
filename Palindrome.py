f765fcae8b2b4a77dfcf7eab20d9287325d45c87
James Croft <jfecroft@Jamess-Air.home>
2014-12-14 15:02:17 +0000

    Modified to 4 space indents for PEP8 consistancy.

--- Palindrome.py
+++ Palindrome.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 def IsPalindrome(String):
- character_deque = deque()
- #put all chracters in deque then check one by one
- for i in String: character_deque.appendleft(i)
+    character_deque = deque()
+    #put all chracters in deque then check one by one
+    for i in String: character_deque.appendleft(i)
 
- Equal = True
- while Equal and len(character_deque) > 1:
-  rightcharacter = character_deque.pop()
-  leftcharacter  = character_deque.popleft()
-  if rightcharacter != leftcharacter: Equal = False #if any not equal then not a palidrome
- return Equal
+    Equal = True
+    while Equal and len(character_deque) > 1:
+        rightcharacter = character_deque.pop()
+        leftcharacter  = character_deque.popleft()
+        if rightcharacter != leftcharacter: Equal = False #if any not equal then not a palidrome
+    return Equal
 
 
