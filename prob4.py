bd9c4cbde3f3484de8336a06fdf15d2dae067766
James Croft <jfecroft@Jamess-MacBook-Air.local>
2014-12-14 21:32:33 +0000

    Changed loop variables from i and j to num1 and num2 for clarity.

--- prob4.py
+++ prob4.py
@@ -3,8 +3,8 @@ from Palindrome import is_palindrome
 
 MAX = 999
 highest = 0
-for i, j in product(range(MAX+1), range(MAX+1)):
- if is_palindrome(str(i*j)) and i*j > highest:
-  highest = i*j
+for num1, num2 in product(range(MAX+1), range(MAX+1)):
+ if is_palindrome(str(num1*num2)) and num1*num2 > highest:
+  highest = num1*num2
 
 print highest
