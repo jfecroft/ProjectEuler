6164ce4011f582b69c3b3103323204d0ab9ca985
James Croft <jfecroft@Jamess-MacBook-Air.local>
2014-12-14 21:09:35 +0000

    Updated prob1.py to comply with PEP8.

--- prob1.py
+++ prob1.py
@@ -1,17 +1,15 @@
-UpperLimit = 1000
+UPPER_LIMIT = 1000
 
-def MultipleOfThreeOrFive(num):
- if num%3 == 0:
-  return True
- elif num%5 ==0:
-  return True
- else:
-  return False
-
+def multiple_of_three_or_five(num):
+    if num%3 == 0:
+        return True
+    elif num%5 ==0:
+        return True
+    else:
+        return False
 
 sum = 0
-for i in range(1,UpperLimit):
- if MultipleOfThreeOrFive(i): sum += i 
-
+for i in range(1,UPPER_LIMIT):
+    if multiple_of_three_or_five(i): sum += i 
 
 print sum 
