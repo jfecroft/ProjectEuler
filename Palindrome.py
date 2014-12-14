f0fece503c9385f0d9d3ae0c8b1e9acd82b1b724
James Croft <jfecroft@Jamess-Air.home>
2014-12-14 15:05:33 +0000

    Updated comments and added docstring.

--- Palindrome.py
+++ Palindrome.py
@@ -1,15 +1,18 @@
 from collections import deque
 
-def IsPalindrome(String):
-    character_deque = deque()
-    #put all chracters in deque then check one by one
-    for i in String: character_deque.appendleft(i)
+def IsPalindrome(string):
+    """Check if string is a palindrome."""
 
+    character_deque = deque()
+    # Put all chracters in deque.
+    for i in string: character_deque.appendleft(i)
+
+    # Check if characters in deque are symmetric.
     Equal = True
     while Equal and len(character_deque) > 1:
         rightcharacter = character_deque.pop()
         leftcharacter  = character_deque.popleft()
-        if rightcharacter != leftcharacter: Equal = False #if any not equal then not a palidrome
+        if rightcharacter != leftcharacter: Equal = False # Not a palidrome
     return Equal
 
 
