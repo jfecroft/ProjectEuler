726567a7379f8bc8c983383affe06687b45df89b
James Croft <jfecroft@Jamess-MacBook-Air.local>
2014-12-14 21:13:01 +0000

    Updated prob2.py to comply with PEP8.

--- prob2.py
+++ prob2.py
@@ -1,7 +1,8 @@
 sum = 0
-a,b = 0,1
-while b < 4000000:
- a, b = b, a + b
- if b%2 == 0: sum += b
+a, b = 0, 1
+UPPER_LIMIT = 4000000
+while b < UPPER_LIMIT:
+    a, b = b, a+b
+    if b%2 == 0: sum += b
 
 print sum
