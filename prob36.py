32752f3a05fdf38d4c599c77852d3317d6faf38a
James Croft <jfecroft@Jamess-MacBook-Air.local>
2014-12-15 08:19:40 +0000

    Updated prob36.py to adhere to PEP8.

--- prob36.py
+++ prob36.py
@@ -3,15 +3,16 @@ from Palindrome import is_palindrome
 
 NumMax = 1000000
 Sum = 0
-PalindromesInBase2=[]
-#check to see if palindrome in base10
-for Num in xrange(NumMax):
- if is_palindrome(str(Num)):
-  PalindromesInBase2.append(Num)
+palindromes = []
 
-#if palindrome in base 2 see if also palindrome in base2
-for Num in  PalindromesInBase2:
- if is_palindrome(binary_repr(Num)):
-  Sum += Num
+# Check to see if palindrome in base10.
+for Num in xrange(NumMax):
+    if is_palindrome(str(Num)):
+        palindromes.append(Num)
+
+# If palindrome in base10 see if also palindrome in base2.
+for Num in  palindromes:
+    if is_palindrome(binary_repr(Num)):
+        Sum += Num
 
 print Sum
