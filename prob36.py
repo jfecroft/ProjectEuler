3fcfb805b6ff109947946124e116ee95028b479e
James Croft <jfecroft@Jamess-MacBook-Air.local>
2014-12-15 08:28:19 +0000

    Now complies with PEP8.

--- prob36.py
+++ prob36.py
@@ -1,18 +1,18 @@
 from numpy import binary_repr
 from Palindrome import is_palindrome
 
-NumMax = 1000000
-Sum = 0
+MAX_NUMBER = 1000000
+sum_ = 0
 palindromes = []
 
 # Check to see if palindrome in base10.
-for Num in xrange(NumMax):
-    if is_palindrome(str(Num)):
-        palindromes.append(Num)
+for number in xrange(MAX_NUMBER+1):
+    if is_palindrome(str(number)):
+        palindromes.append(number)
 
 # If palindrome in base10 see if also palindrome in base2.
-for Num in  palindromes:
-    if is_palindrome(binary_repr(Num)):
-        Sum += Num
+for number in  palindromes:
+    if is_palindrome(binary_repr(number)):
+        sum_ += number
 
-print Sum
+print sum_
