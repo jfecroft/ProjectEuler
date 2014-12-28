302c32ebd8b88fdd74c64077c4043cad94f24609
James Croft <jfecroft@Jamess-MacBook-Air.local>
2014-12-28 23:47:49 +0000

    Completed solution to problem 27.

--- prob27.py
+++ prob27.py
@@ -1,9 +1,29 @@
-from PrimeFactorisation import is_prime 
+from PrimeFactorisation import is_prime
+from itertools import product
 
 
 def quadratic_formula(a, b, n):
     return is_prime(n**2 + a*n + b)
 
 
-for i in xrange(50):
-    print i, quadratic_formula(1, 41, i)
+def consecutive_solutions(a, b):
+    prime_test = True
+    n = 0
+    while prime_test is True:
+        prime_test = quadratic_formula(a, b, n)
+        n += 1
+    return n - 2
+
+
+A_MAX = 1000
+B_MAX = 1000
+
+maximum = 0
+
+for a, b in product(xrange(-A_MAX+1, A_MAX), xrange(-B_MAX+1, B_MAX)):
+    test_maximum = consecutive_solutions(a, b)
+    if test_maximum > maximum:
+        maximum = test_maximum
+        ab = (a, b)
+
+print ab[0]*ab[1]
