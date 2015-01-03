e61f5b21de3c3214cd2d2df39b3935ba6bfb8968
jfecroft <j.f.e.croft@gmail.com>
2015-01-03 10:00:31 +0000

    [cl_pep8]

--- prob21.py
+++ prob21.py
@@ -1,17 +1,20 @@
-from PrimeFactorisation import ProperDivisors
+from PrimeFactorisation import get_proper_divisors
 import numpy as np
 
-def d(n):
- PD = np.array(list(ProperDivisors(n)))
- return np.sum(PD)
 
-low, high = 2, 10000  #start at 2 to avoid empty set for proper divisors for 1
-sum_divisors = [d(n) for n in xrange(low,high+1)] #get d(n)  for n
+def proper_divisor_sum(integer):
+    """Return the sum of the proper divisors of *n*."""
+    proper_divisors = np.array(list(get_proper_divisors(integer)))
+    return np.sum(proper_divisors)
 
-sum = 0 
-for i, j  in enumerate(sum_divisors):
-  if i+low < j and j-low >= 0 and j-low <= len(sum_divisors)-1 and sum_divisors[j-low] == i+low:
-   sum += i+low 
-   sum += j 
+LOW, HIGH = 2, 10000  # start at 2 to avoid empty set for proper divisors for 1
+sum_divisors = [proper_divisor_sum(x) for x in xrange(LOW, HIGH + 1)]
 
-print sum
+sum_amicable_numbers = 0
+for i, j in enumerate(sum_divisors):
+    if (i+LOW < j and j-LOW >= 0 and j-LOW <= len(sum_divisors)-1 and
+            sum_divisors[j-LOW] == i+LOW):
+        sum_amicable_numbers += i+LOW
+        sum_amicable_numbers += j
+
+print sum_amicable_numbers
