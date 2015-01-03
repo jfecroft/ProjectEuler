000f7b9f249ffd67522a53b8af178f5054076890
jfecroft <j.f.e.croft@gmail.com>
2015-01-03 09:11:21 +0000

    [cl_pep8] fix indentation.

--- prob4.py
+++ prob4.py
@@ -1,10 +1,11 @@
+"""Solves project euler problem 4."""
 from itertools import product
 from Palindrome import is_palindrome
 
 MAX = 999
 highest = 0
 for num1, num2 in product(range(MAX+1), range(MAX+1)):
- if is_palindrome(str(num1*num2)) and num1*num2 > highest:
-  highest = num1*num2
+    if is_palindrome(str(num1*num2)) and num1*num2 > highest:
+        highest = num1*num2
 
 print highest
