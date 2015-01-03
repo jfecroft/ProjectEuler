d1b3bd3f7d19410085f11bae6a7dcfc219ca6560
jfecroft <j.f.e.croft@gmail.com>
2015-01-03 09:16:53 +0000

    [cl_pep8]

--- prob1.py
+++ prob1.py
@@ -1,15 +1,19 @@
+"""Solves project euler problem 1."""
 UPPER_LIMIT = 1000
 
+
 def multiple_of_three_or_five(num):
-    if num%3 == 0:
+    """Ruturn if *num* is a multiple of 3 or 5."""
+    if num % 3 == 0:
         return True
-    elif num%5 ==0:
+    elif num % 5 == 0:
         return True
     else:
         return False
 
-sum = 0
-for i in range(1,UPPER_LIMIT):
-    if multiple_of_three_or_five(i): sum += i 
+sum_multiples = 0
+for i in range(1, UPPER_LIMIT):
+    if multiple_of_three_or_five(i):
+        sum_multiples += i
 
-print sum 
+print sum_multiples
