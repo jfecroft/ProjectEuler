91e27ac9b4ee5edac43291f5e2cb9d6b2c84a85d
jfecroft <j.f.e.croft@gmail.com>
2015-01-14 21:29:46 +0000

    [cl_pep8]

--- prob25.py
+++ prob25.py
@@ -1,19 +1,24 @@
+"""
+solve project euler problem 25.
+"""
+
 
 def fib():
- a,b = 0,1
- while True:
-  a,b = b,a+b
-  yield b
+    """
+    fibonacci number generator.
+    """
+    a, b = 0, 1
+    while True:
+        a, b = b, a+b
+        yield b
 
-NumDigits = 3
-NumDigits = 1000
+number_of_digits = 1000
 
-FibGen = fib()
-FibNum = 0
+fib_generator = fib()
+fib_number = 0
 term = 1
-while len(str(FibNum)) < NumDigits:
- FibNum = FibGen.next()
- term += 1
+while len(str(fib_number)) < number_of_digits:
+    fib_number = fib_generator.next()
+    term += 1
 
-print term 
- 
+print term
