54e723be4e320059e4128755efc3961fb16af8a6
jfecroft <j.f.e.croft@gmail.com>
2015-01-14 21:25:06 +0000

    [cl_pep8]

--- prob14.py
+++ prob14.py
@@ -1,33 +1,39 @@
-def FindInListofLists(data,search):
- """finds the search in list of lists"""
- #should in general be recursive
- for sublist in data:
-     if search in sublist:
-         return sublist
+def FindInListofLists(data, search):
+    """
+    find *search* in list of lists *data*
+    """
+    #  should in general be recursive
+    for sublist in data:
+        if search in sublist:
+            return sublist
 
-def collatz(n,CollatzList=None,AlreadyComputed=[[]]):
- #AlreadyComputed is a mutable default as such will always update whenever used
- if CollatzList == None: CollatzList=[]
- CollatzList.append(n)
- KnownPath = FindInListofLists(AlreadyComputed,n)
- if not KnownPath == None:
-  CollatzList.extend(KnownPath[KnownPath.index(n)+1:])
-  AlreadyComputed.remove(KnownPath) 
-  AlreadyComputed.append(CollatzList)
-  return CollatzList 
- if n ==1 : return CollatzList 
- if n%2 == 0: return collatz(n/2,CollatzList)
- else: return collatz(3*n+1,CollatzList)
 
+def collatz(n, CollatzList=None, AlreadyComputed=[[]]):
+    #  AlreadyComputed is a mutable default will always update whenever used
+    if CollatzList is None:
+        CollatzList = []
+    CollatzList.append(n)
+    KnownPath = FindInListofLists(AlreadyComputed, n)
+    if KnownPath is not None:
+        CollatzList.extend(KnownPath[KnownPath.index(n)+1:])
+        AlreadyComputed.remove(KnownPath)
+        AlreadyComputed.append(CollatzList)
+        return CollatzList
+    if n == 1:
+        return CollatzList
+    if n % 2 == 0:
+        return collatz(n/2, CollatzList)
+    else:
+        return collatz(3*n+1, CollatzList)
 
 
 Max = 0
 NumMax = 1000000
-for i in xrange(2,NumMax):
- CollatzList = collatz(i,None)
- length = len(CollatzList)
- if length > Max:
-  Max = length
-  StartNum = CollatzList[0]
+for i in xrange(2, NumMax):
+    CollatzList = collatz(i, None)
+    length = len(CollatzList)
+    if length > Max:
+        Max = length
+        StartNum = CollatzList[0]
 
-print StartNum,Max
+print StartNum, Max
