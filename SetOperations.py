f8dfb70acd13bf2ec4954bc93eab9a1df720e4de
jfecroft <j.f.e.croft@gmail.com>
2015-01-14 22:59:36 +0000

    add unittest for setoperations.

--- SetOperations.py
+++ SetOperations.py
@@ -6,5 +6,6 @@ def powerset(iterable):
     return the powerset of *iterable*
     """
     xs = list(iterable)
+    #  should return a set not a list
     return list(chain.from_iterable(combinations(xs, n) for
                 n in range(len(xs) + 1)))
