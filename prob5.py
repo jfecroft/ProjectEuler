017cd680fc2a60ff441378b9da79d2dcd929b5ef
jfecroft <j.f.e.croft@gmail.com>
2015-01-26 08:54:30 +0000

    [pep8]

--- prob5.py
+++ prob5.py
@@ -1,9 +1,9 @@
-def is_multiple(n,numbers):
-    return all(n%number == 0 for number in numbers)
+def is_multiple(n, numbers):
+    return all(n % number == 0 for number in numbers)
 
 MAX_NUMBER = 20
-number = MAX_NUMBER # Start at MAX_NUMBER.
+number = MAX_NUMBER  # Start at MAX_NUMBER.
 while not is_multiple(number, range(1, MAX_NUMBER+1)):
-    number += MAX_NUMBER # Check all numbers multiples of MAX_NUMBER.
+    number += MAX_NUMBER  # Check all numbers multiples of MAX_NUMBER.
 
 print number
