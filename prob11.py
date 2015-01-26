0ab5f4a2348bf388c65d5aaa4ba92e640c75eb7c
jfecroft <j.f.e.croft@gmail.com>
2015-01-26 08:49:27 +0000

    [pep8]

--- prob11.py
+++ prob11.py
@@ -8,14 +8,16 @@ maximum = 0
 
 while not index_iterator.finished:
     index = index_iterator.multi_index
-    sub_grid = grid[index[0]:index[0]+number_adjacent, index[1]:index[1]+number_adjacent]
-    if (number_adjacent, number_adjacent) == np.shape(sub_grid): # To account for corners.
-        up =  np.prod(sub_grid[:,0])
-        along =  np.prod(sub_grid[0,:])
-        diag1 =  np.prod(np.diag(sub_grid))
-        diag2 =  np.prod(np.diag(sub_grid[::-1]))
+    sub_grid = grid[index[0]:index[0]+number_adjacent,
+                    index[1]:index[1]+number_adjacent]
+    # To account for corners.
+    if (number_adjacent, number_adjacent) == np.shape(sub_grid):
+        up = np.prod(sub_grid[:, 0])
+        along = np.prod(sub_grid[0, :])
+        diag1 = np.prod(np.diag(sub_grid))
+        diag2 = np.prod(np.diag(sub_grid[::-1]))
         if np.amax((up, along, diag1, diag2)) > maximum:
             maximum = np.amax((up, along, diag1, diag2))
     index_iterator.iternext()
 
-print maximum 
+print maximum
