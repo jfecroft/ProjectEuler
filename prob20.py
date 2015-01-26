5e8f995aede153274bf994c0e7497833c2722846
jfecroft <j.f.e.croft@gmail.com>
2015-01-26 08:34:13 +0000

    housekeeping

--- prob20.py
+++ prob20.py
@@ -1,5 +1,3 @@
 from math import factorial
 digitstring = str(factorial(100))
-digits = [int(digitstring[i]) for i in range(len(digitstring))]
-print sum(digits)
-
+print sum((int(digitstring[i]) for i in range(len(digitstring))))
