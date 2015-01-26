b1f02063337c32d0c5e2ab7f37cb552c0cfe6ea6
jfecroft <j.f.e.croft@gmail.com>
2015-01-26 08:52:24 +0000

    [pep8]

--- prob3.py
+++ prob3.py
@@ -1,9 +1,10 @@
 prime = 600851475143
 
+
 def largest_prime_factor(n):
     """ Return the largest prime factor of n."""
     i = 2
-    while (n%i != 0 and i < n):
+    while (n % i != 0 and i < n):
         i += 1
     if (i < n):
         return largest_prime_factor(n/i)
