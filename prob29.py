919fa4d85230dc4630fb7a1831d7eab9733ec78f
jfecroft <j.f.e.croft@gmail.com>
2015-01-26 08:51:28 +0000

    [pep8]

--- prob29.py
+++ prob29.py
@@ -1,13 +1,13 @@
 import itertools
 
-LowLim =2
-UpLim =100
+LowLim = 2
+UpLim = 100
 
-A = range(LowLim,UpLim+1)
-B = range(LowLim,UpLim+1)
+A = range(LowLim, UpLim+1)
+B = range(LowLim, UpLim+1)
 
 DistinctTerms = set()
-for a,b in itertools.product(A,B):
- DistinctTerms.add(a**b)
+for a, b in itertools.product(A, B):
+    DistinctTerms.add(a**b)
 
 print len(DistinctTerms)
