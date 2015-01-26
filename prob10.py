1849f3266c88fd2ff2b58affac62e48c0b3333d4
jfecroft <j.f.e.croft@gmail.com>
2015-01-26 08:46:53 +0000

    [pep8]

--- prob10.py
+++ prob10.py
@@ -1,25 +1,26 @@
 import numpy as np
 from math import sqrt
-NumMax = 2000000 
+NumMax = 2000000
 
 
 def PrimesLessThanN(NumMax):
- PrimesList =[]
- Primes = [True]*NumMax
- i=2
- while i < sqrt(NumMax):
-  if Primes[i] is True:
-   j = i**2
-   while j < NumMax:
-    Primes[j] = False
-    j += i
-  i += 1
- 
- for i in range(2,len(Primes)):
-  if Primes[i] == True: PrimesList.append(i)
- return PrimesList
+    PrimesList = []
+    Primes = [True]*NumMax
+    i = 2
+    while i < sqrt(NumMax):
+        if Primes[i] is True:
+            j = i**2
+            while j < NumMax:
+                Primes[j] = False
+                j += i
+        i += 1
+
+    for i in range(2, len(Primes)):
+        if Primes[i] is True:
+            PrimesList.append(i)
+    return PrimesList
 
 
-PrimesList = PrimesLessThanN(NumMax) 
-PrimesList= np.array(PrimesList)
+PrimesList = PrimesLessThanN(NumMax)
+PrimesList = np.array(PrimesList)
 print np.sum(PrimesList)
