ef0749d5cfe0d9eed88aeb45fa8aaec1f70b8cef
jfecroft <j.f.e.croft@gmail.com>
2015-03-07 13:39:26 +0000

    [cl] pep8

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,19 @@
 from collections import deque
 
+
 def is_palindrome(string):
     """ Return if a string is a palindrome."""
 
     character_deque = deque()
     # Put all chracters in deque.
-    for i in string: character_deque.appendleft(i)
+    for i in string:
+        character_deque.appendleft(i)
 
     # Check if characters in deque are symmetric.
-    Equal = True
-    while Equal and len(character_deque) > 1:
+    equal = True
+    while equal and len(character_deque) > 1:
         rightcharacter = character_deque.pop()
-        leftcharacter  = character_deque.popleft()
-        if rightcharacter != leftcharacter: Equal = False # Not a palidrome
-    return Equal
-
-
+        leftcharacter = character_deque.popleft()
+        if rightcharacter != leftcharacter:
+            equal = False  # Not a palidrome
+    return equal
