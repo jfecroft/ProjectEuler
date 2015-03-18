6dda50d685b27f2853426c2b8057cee83086f82a
jfecroft <j.f.e.croft@gmail.com>
2015-03-18 20:46:18 +0000

    [pep8]

--- prob49.py
+++ prob49.py
@@ -4,10 +4,10 @@ solve project euler problem 49
 
 from PrimeFactorisation import is_prime
 
-characters = lambda x: sorted(list(str(x)))
+CHARACTERS = lambda x: sorted(list(str(x)))
 
 for a in xrange(1000, 10000):
     for b in xrange(1, (10000-a)/2+1):
         if (is_prime(a) and is_prime(a+b) and is_prime(a+b+b) and
-                characters(a) == characters(a+b) == characters(a+b+b)):
+                CHARACTERS(a) == CHARACTERS(a+b) == CHARACTERS(a+b+b)):
             print a, a+b, a+b+b
