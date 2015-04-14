3e6233bbec539b57c110e2f8b1ce8ba44695b314
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 22:58:00 +0100

    [cl] pylint

--- prob36.py
+++ prob36.py
@@ -1,18 +1,22 @@
+"""
+solve problem 36
+"""
+# pylint: disable-msg=E0611
 from numpy import binary_repr
 from palindrome import is_palindrome
 
 MAX_NUMBER = 1000000
-sum_ = 0
-palindromes = []
+SUM = 0
+PALINDROMES = []
 
 # Check to see if palindrome in base10.
 for number in xrange(MAX_NUMBER+1):
     if is_palindrome(str(number)):
-        palindromes.append(number)
+        PALINDROMES.append(number)
 
 # If palindrome in base10 see if also palindrome in base2.
-for number in palindromes:
+for number in PALINDROMES:
     if is_palindrome(binary_repr(number)):
-        sum_ += number
+        SUM += number
 
-print sum_
+print SUM
