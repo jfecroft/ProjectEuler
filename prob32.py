9b9ef820627e666337455453def237d556d8ee32
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 23:18:32 +0100

    [cl] pylint

--- prob32.py
+++ prob32.py
@@ -3,9 +3,9 @@ solve project euler problem 32
 """
 
 import itertools
-Calc = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-PanDigital = set()
-for combination in itertools.permutations(Calc, len(Calc)):
+CALC = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+PANDIGITAL = set()
+for combination in itertools.permutations(CALC, len(CALC)):
     comb = list(combination)
     for i in range(1, len(combination)):
         for j in range(i+1, len(combination)):
@@ -13,6 +13,6 @@ for combination in itertools.permutations(Calc, len(Calc)):
             b = int(''.join(comb[i:j]))
             c = int(''.join(comb[j:]))
             if a*b == c and a < b:
-                PanDigital.add(c)
+                PANDIGITAL.add(c)
 
-print sum(PanDigital)
+print sum(PANDIGITAL)
