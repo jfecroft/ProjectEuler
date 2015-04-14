c06cb21902fc5e373d2a1f4939d1c1e1f128b120
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 21:29:02 +0100

    [cl] pep8

--- prime_factorisation.py
+++ prime_factorisation.py
@@ -1,7 +1,12 @@
-import numpy as np
+"""
+prime number methods
+"""
 
+import numpy as np
 from set_operations import powerset
 from math import sqrt
+
+# pylint: disable=E1103
 
 
 def is_prime(number):
@@ -18,49 +23,49 @@ def is_prime(number):
     return False
 
 
-def get_proper_divisors(n):
-    """Return the proper divisors of *n*."""
-    proper_divisors = powerset(prime_factors(n))
+def get_proper_divisors(num):
+    """Return the proper divisors of *num*."""
+    proper_divisors = powerset(prime_factors(num))
     proper_divisors.remove(())
     for i, j in enumerate(proper_divisors):
         proper_divisors[i] = np.prod(np.array(j))
     proper_divisors = set(proper_divisors)
-    proper_divisors.remove(n)
+    proper_divisors.remove(num)
     return proper_divisors
 
 
-def prime_factors(n, primes_list=None):
+def prime_factors(num, primes_list=None):
     """returns the prime factorisation of *n*."""
     if primes_list is None:
         primes_list = []
-    if n == 1:
+    if num == 1:
         return []  # account for empty set for 1
     i = 2
-    while n % i != 0 and i < n:
+    while num % i != 0 and i < num:
         i += 1
     if i > 1:
         primes_list.append(i)
-    if i < n:
-        return prime_factors(n/i, primes_list)
+    if i < num:
+        return prime_factors(num/i, primes_list)
     else:
         primes_list.append(1)
         return primes_list
 
 
-def primes_less_than_n(NumMax):
-    """Return primes less than *NumMax*."""
+def primes_less_than_n(num):
+    """Return primes less than *num*."""
     primes_list = []
-    Primes = [True]*NumMax
+    primes = [True]*num
     i = 2
-    while i < sqrt(NumMax):
-        if Primes[i] is True:
+    while i < sqrt(num):
+        if primes[i] is True:
             j = i**2
-            while j < NumMax:
-                Primes[j] = False
+            while j < num:
+                primes[j] = False
                 j += i
         i += 1
 
-    for i in range(2, len(Primes)):
-        if Primes[i] is True:
+    for i in range(2, len(primes)):
+        if primes[i] is True:
             primes_list.append(i)
     return primes_list
