36d127c1fa9546b08bc2372170cb4c651fe7ac43
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 22:08:16 +0100

    [cl] pep8

--- prob3.py
+++ prob3.py
@@ -1,14 +1,18 @@
-prime = 600851475143
+"""
+solve problem 3
+"""
+
+PRIME = 600851475143
 
 
-def largest_prime_factor(n):
+def largest_prime_factor(num):
     """ Return the largest prime factor of n."""
     i = 2
-    while (n % i != 0 and i < n):
+    while num % i != 0 and i < num:
         i += 1
-    if (i < n):
-        return largest_prime_factor(n/i)
+    if i < num:
+        return largest_prime_factor(num/i)
     else:
-        return n
+        return num
 
-print largest_prime_factor(prime)
+print largest_prime_factor(PRIME)
