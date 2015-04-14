dfcd745efe5bca16d5f2fdf0f8809da6cef5917f
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 22:53:07 +0100

    [cl] pylint

--- prob12.py
+++ prob12.py
@@ -1,32 +1,43 @@
+"""
+solve problem 12
+"""
 from prime_factorisation import prime_factors
 import itertools
 import numpy as np
 
+# pylint: disable=E1103
+
 
 def get_combintions(lst):
+    """
+    return all cobinations of the elements in lst
+    """
     combs = []
-    for i in xrange(1, len(lst)+1):
-        els = [list(x) for x in itertools.combinations(lst, i)]
+    for iii in xrange(1, len(lst)+1):
+        els = [list(x) for x in itertools.combinations(lst, iii)]
         combs.extend(els)
     return combs
 
 
 def generate_triangle_numbers():
-    i = 0
+    """
+    return triangle numbers
+    """
+    iii = 0
     tri = 0
     while True:
-        i += 1
-        tri += i
+        iii += 1
+        tri += iii
         yield tri
 
 
-triangle_number_generator = generate_triangle_numbers()
-number_of_factors = 0
-while number_of_factors < 500:
-    triangle_number = triangle_number_generator.next()
-    Factors = prime_factors(triangle_number)
-    combintions = get_combintions(Factors)
-    combintions = [np.prod(i) for i in combintions]
-    number_of_factors = len(set(combintions))
+TRIANGLE_NUMBER_GENERATOR = generate_triangle_numbers()
+NUMBER_OF_FACTORS = 0
+while NUMBER_OF_FACTORS < 500:
+    TRIANGLE_NUMBER = TRIANGLE_NUMBER_GENERATOR.next()
+    FACTORS = prime_factors(TRIANGLE_NUMBER)
+    COMBINTIONS = get_combintions(FACTORS)
+    COMBINTIONS = [np.prod(i) for i in COMBINTIONS]
+    NUMBER_OF_FACTORS = len(set(COMBINTIONS))
 
-print triangle_number
+print TRIANGLE_NUMBER
