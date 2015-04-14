b7e6a7fc22395f4d42cda3392e7b931195da8b14
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 23:05:39 +0100

    [cl] pylint

--- prob39.py
+++ prob39.py
@@ -1,17 +1,23 @@
+"""
+solve problem 39
+"""
 from operator import itemgetter
 
 
 def integer_right_triangles(perimeter):
+    """
+    return right angle triangles with integer side and perimeter
+    """
     count = 0
-    for a in xrange(1, perimeter/2+1):
-        for b in xrange(a+1, perimeter-a):
-            c = perimeter - a - b
-            if a**2 + b**2 == c**2:
+    for aaa in xrange(1, perimeter/2+1):
+        for bbb in xrange(aaa+1, perimeter-aaa):
+            ccc = perimeter - aaa - bbb
+            if aaa**2 + bbb**2 == ccc**2:
                 count += 1
     return count
 
-maximum = max(
+MAXIMUM = max(
     ((i, integer_right_triangles(i)) for i in xrange(1001)),
     key=itemgetter(1)
 )
-print maximum[0]
+print MAXIMUM[0]
