fefcf51fc7a94cd9a29d1baca630bcaddb2e7ff4
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 23:00:04 +0100

    [cl] pylint

--- prob27.py
+++ prob27.py
@@ -4,26 +4,26 @@ from prime_factorisation import is_prime
 from itertools import product
 
 
-def consecutive_solutions(a, b):
+def consecutive_solutions(aaa, bbb):
     """Return the number of consective primes
-    given by n**2 + a*n + b for *a* and *b* starting from
+    given by n**2 + a*n + b for *aaa* and *bbb* starting from
     n = 0.
     """
-    n = 0
-    while is_prime(n**2 + a*n + b):
-        n += 1
-    return n - 2  # -2 accounts for 0 and extra n += 1 in while loop.
+    num = 0
+    while is_prime(num**2 + aaa*num + bbb):
+        num += 1
+    return num - 2  # -2 accounts for 0 and extra n += 1 in while loop.
 
 
 A_MAX = 1000
 B_MAX = 1000
 
-maximum = 0
+MAXIMUM = 0
 
 for a, b in product(xrange(-A_MAX+1, A_MAX), xrange(-B_MAX+1, B_MAX)):
     test_maximum = consecutive_solutions(a, b)
-    if test_maximum > maximum:
-        maximum = test_maximum
+    if test_maximum > MAXIMUM:
+        MAXIMUM = test_maximum
         ab = (a, b)
 
 print ab[0]*ab[1]
