e8e71eb13476fd92266a52bfea966c052b6cb786
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 22:48:15 +0100

    [cl] pylint

--- prob21.py
+++ prob21.py
@@ -1,5 +1,11 @@
+"""
+solve problem 21
+"""
 from prime_factorisation import get_proper_divisors
 import numpy as np
+
+
+# pylint: disable=E1103
 
 
 def proper_divisor_sum(integer):
@@ -8,13 +14,13 @@ def proper_divisor_sum(integer):
     return np.sum(proper_divisors)
 
 LOW, HIGH = 2, 10000  # start at 2 to avoid empty set for proper divisors for 1
-sum_divisors = [proper_divisor_sum(x) for x in xrange(LOW, HIGH + 1)]
+SUM_DIVISORS = [proper_divisor_sum(x) for x in xrange(LOW, HIGH + 1)]
 
-sum_amicable_numbers = 0
-for i, j in enumerate(sum_divisors):
-    if (i+LOW < j and j-LOW >= 0 and j-LOW <= len(sum_divisors)-1 and
-            sum_divisors[j-LOW] == i+LOW):
-        sum_amicable_numbers += i+LOW
-        sum_amicable_numbers += j
+SUM_AMICABLE_NUMBERS = 0
+for i, j in enumerate(SUM_DIVISORS):
+    if (i+LOW < j and j-LOW >= 0 and j-LOW <= len(SUM_DIVISORS)-1 and
+            SUM_DIVISORS[j-LOW] == i+LOW):
+        SUM_AMICABLE_NUMBERS += i+LOW
+        SUM_AMICABLE_NUMBERS += j
 
-print sum_amicable_numbers
+print SUM_AMICABLE_NUMBERS
