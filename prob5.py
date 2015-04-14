ce9d86a249c4106c54c5011baeb2127afd2fbc78
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 23:14:17 +0100

    [cl] pylint

--- prob5.py
+++ prob5.py
@@ -1,9 +1,17 @@
-def is_multiple(n, numbers):
-    return all(n % number == 0 for number in numbers)
+"""
+solve problem 5
+"""
+
+
+def is_multiple(num, numbers):
+    """
+    return if numbers are all multiples of num
+    """
+    return all(num % number == 0 for number in numbers)
 
 MAX_NUMBER = 20
-number = MAX_NUMBER  # Start at MAX_NUMBER.
-while not is_multiple(number, range(1, MAX_NUMBER+1)):
-    number += MAX_NUMBER  # Check all numbers multiples of MAX_NUMBER.
+NUMBER = MAX_NUMBER  # Start at MAX_NUMBER.
+while not is_multiple(NUMBER, range(1, MAX_NUMBER+1)):
+    NUMBER += MAX_NUMBER  # Check all numbers multiples of MAX_NUMBER.
 
-print number
+print NUMBER
