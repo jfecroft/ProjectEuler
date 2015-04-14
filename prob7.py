0204e4129246c092a35b8682e14f1997711faf8e
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 22:12:04 +0100

    [cl] pylint

--- prob7.py
+++ prob7.py
@@ -1,30 +1,23 @@
-import math
-
-
-def is_prime(number):
-    if number > 1:
-        if number == 2:
-            return True
-        if number % 2 == 0:
-            return False
-        for current in range(3, int(math.sqrt(number) + 1), 2):
-            if number % current == 0:
-                return False
-        return True
-    return False
+"""
+solve problem 7
+"""
+from prime_factorisation import is_prime
 
 
 def get_primes(number):
+    """
+    return prime number in order
+    """
     while True:
         if is_prime(number):
             yield number
         number += 1
 
 
-NumMax = 10001
+MAX_NUM = 10001
 
-GetNextPrime = get_primes(1)
-for i in range(NumMax):
-    HighPrime = GetNextPrime.next()
+PRIME_GEN = get_primes(1)
+for i in range(MAX_NUM):
+    HighPrime = PRIME_GEN.next()
 
 print HighPrime
