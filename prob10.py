c3cb5065e190da9284f2ef8b368d84e3f6179d8a
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 21:42:06 +0100

    [cl] pep8

--- prob10.py
+++ prob10.py
@@ -1,26 +1,12 @@
+"""
+solve problem 10
+"""
 import numpy as np
-from math import sqrt
-NumMax = 2000000
+from prime_factorisation import primes_less_than_n
+MAX_NUM = 2000000
 
+# pylint: disable=E1103
 
-def primes_less_than_n(NumMax):
-    PrimesList = []
-    Primes = [True]*NumMax
-    i = 2
-    while i < sqrt(NumMax):
-        if Primes[i] is True:
-            j = i**2
-            while j < NumMax:
-                Primes[j] = False
-                j += i
-        i += 1
-
-    for i in range(2, len(Primes)):
-        if Primes[i] is True:
-            PrimesList.append(i)
-    return PrimesList
-
-
-PrimesList = primes_less_than_n(NumMax)
-PrimesList = np.array(PrimesList)
-print np.sum(PrimesList)
+PRIMES_LIST = primes_less_than_n(MAX_NUM)
+PRIMES_LIST = np.array(PRIMES_LIST)
+print np.sum(PRIMES_LIST)
