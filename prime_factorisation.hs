331db3f51923dc536216020fb7381f906424322d
jfecroft <j.f.e.croft@gmail.com>
2015-04-19 13:56:42 +0100

    [cl] hlint

--- prime_factorisation.hs
+++ prime_factorisation.hs
@@ -1,5 +1,5 @@
-prime_factors n =
+primeFactors n =
   case factors of
     [] -> [n]
-    _  -> factors ++ prime_factors (n `div` (head factors))
+    _  -> factors ++ primeFactors (n `div` head factors)
   where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
