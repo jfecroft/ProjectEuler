a9ade2e5751f3f6ac196f638e96fc4318d47858d
jfecroft <j.f.e.croft@gmail.com>
2015-04-19 12:06:03 +0100

    refactor for clarity

--- prob1.py
+++ prob1.py
@@ -1,19 +1,3 @@
 """Solves project euler problem 1."""
 UPPER_LIMIT = 1000
-
-
-def multiple_of_three_or_five(num):
-    """Ruturn if *num* is a multiple of 3 or 5."""
-    if num % 3 == 0:
-        return True
-    elif num % 5 == 0:
-        return True
-    else:
-        return False
-
-SUM_MULTIPLES = 0
-for i in range(1, UPPER_LIMIT):
-    if multiple_of_three_or_five(i):
-        SUM_MULTIPLES += i
-
-print SUM_MULTIPLES
+print sum(x for x in range(UPPER_LIMIT) if x % 3 == 0 or x % 5 == 0)
