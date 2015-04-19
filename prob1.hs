aa5a08b4d01578388ac03fbb464dcc69e6b4b700
jfecroft <j.f.e.croft@gmail.com>
2015-04-19 12:00:48 +0100

    [cl] hlint

--- prob1.hs
+++ prob1.hs
@@ -1,2 +1,4 @@
 main :: IO ()
-main = print (sum [x |x <- [0..1000-1] ,(x `mod` 3 == 0) || (x `mod` 5 == 0)])
+main = print problem_1 
+problem_1 :: Integer
+problem_1 = sum [x |x <- [0..1000-1] ,(x `mod` 3 == 0) || (x `mod` 5 == 0)]
