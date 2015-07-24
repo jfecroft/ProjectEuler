e07883fda96fd68e9694cb99b976a8033f180909
jfecroft <j.f.e.croft@gmail.com>
2015-07-24 21:00:47 +0100

    [pe] write non shit collatz

--- collatz.py
+++ collatz.py
@@ -1,24 +1,15 @@
-def collatz(num, collatz_list=None, already_computed=None):
+def collatz(num):
     """
     return the collatz path for number
     """
-    if collatz_list is None:
-        collatz_list = []
-    if already_computed is None:
-        already_computed = [[]]
-    collatz_list.append(num)
-    known_path = find_in_list_of_lists(already_computed, num)
-    if known_path is not None:
-        collatz_list.extend(known_path[known_path.index(num)+1:])
-        already_computed.remove(known_path)
-        already_computed.append(collatz_list)
-        return collatz_list
+    collatz_list = [num]
     if num == 1:
-        return collatz_list
-    if num % 2 == 0:
-        return collatz(num/2, collatz_list)
+        pass
+    elif num % 2 == 0:
+        collatz_list.extend(collatz(num/2))
     else:
-        return collatz(3*num+1, collatz_list)
+        collatz_list.extend(collatz(3*num+1))
+    return collatz_list
 
 def find_in_list_of_lists(data, search):
     """
