6b9ba59a85d7f16f8cc84c0fadf6153c75d91074
jfecroft <j.f.e.croft@gmail.com>
2015-08-08 01:21:28 +0100

    [cl] pylint

--- prob10.py
+++ prob10.py
@@ -7,6 +7,10 @@ MAX_NUM = 2000000
 
 # pylint: disable=E1103
 
-PRIMES_LIST = primes_less_than_n(MAX_NUM)
-PRIMES_LIST = np.array(PRIMES_LIST)
-print np.sum(PRIMES_LIST)
+def main():
+    primes_list = primes_less_than_n(MAX_NUM)
+    primes_list = np.array(primes_list)
+    print np.sum(primes_list)
+
+if __name__ == "__main__":
+    main()
