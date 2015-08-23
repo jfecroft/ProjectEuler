bb5e0ec691ec5e6c1958cef47517402114ac9007
jfecroft <j.f.e.croft@gmail.com>
2015-08-23 22:33:07 +0100

    [pe] cl

--- prob5.hs
+++ prob5.hs
@@ -1,13 +1,13 @@
 import Data.List
 main :: IO ()
-main = print output 
+main = print output
 problem_5 :: Maybe Int
 
 
 isMultiple :: Int -> Int -> Bool
 isMultiple y x = all (==True) [(== 0) $ mod x i |i <-[1..y]]
 problem_5 = findIndex (isMultiple 20) [1..]
-output :: Int
+output :: String
 output = case problem_5 of
-        Just x -> x + 1
-        Nothing -> 0 
+        Just x -> show $ x + 1
+        Nothing -> "Nothing"
