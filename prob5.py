8e1615076cd88cb465eab70001bdb5facc0221bc
jfecroft <j.f.e.croft@gmail.com>
2015-08-28 22:40:30 +0100

    [cl] pylint

--- prob5.py
+++ prob5.py
@@ -9,9 +9,16 @@ def is_multiple(num, numbers):
     """
     return all(num % number == 0 for number in numbers)
 
-MAX_NUMBER = 20
-NUMBER = MAX_NUMBER  # Start at MAX_NUMBER.
-while not is_multiple(NUMBER, range(1, MAX_NUMBER+1)):
-    NUMBER += MAX_NUMBER  # Check all numbers multiples of MAX_NUMBER.
 
-print NUMBER
+def main():
+    """
+    main
+    """
+    max_number = 20
+    number = max_number  # Start at max_number.
+    while not is_multiple(number, range(1, max_number+1)):
+        number += max_number  # Check all numbers multiples of MAX_NUMBER.
+    print number
+
+if __name__ == "__main__":
+    main()
