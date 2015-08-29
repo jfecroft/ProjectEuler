a8027af37fe931ac7187f5412ef8d0a2d1bd7300
jfecroft <j.f.e.croft@gmail.com>
2015-08-29 07:40:36 +0100

    [utils] add loadfile

--- utils.py
+++ utils.py
@@ -3,12 +3,21 @@ project euler utils
 """
 
 
+def open_file(filen):
+    """
+    open file and return lines
+    """
+    with open(filen) as ofile:
+        lines = ofile.readlines()
+    return lines
+
+
 def is_palindrome(string):
     """ Return if a string is a palindrome."""
 
     if not isinstance(string, str):
         string = str(string)
-    if len(string) == 1:
+    if len(string) in (1, 0):
         return True
     elif string[0] == string[-1]:
         return is_palindrome(string[1:-1])
