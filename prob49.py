7ff2922eb8c2a2d8a2ea18d08654b7c692e92254
jfecroft <j.f.e.croft@gmail.com>
2015-09-13 17:07:08 +0100

    [cl]

--- prob49.py
+++ prob49.py
@@ -1,13 +1,17 @@
 """
 solve project euler problem 49
 """
-
 from prime_factorisation import is_prime
 
-CHARACTERS = lambda x: sorted(list(str(x)))
+
+def characters(string):
+    """
+    return characters in a string
+    """
+    return sorted(list(str(string)))
 
 for a in xrange(1000, 10000):
     for b in xrange(1, (10000-a)/2+1):
         if (is_prime(a) and is_prime(a+b) and is_prime(a+b+b) and
-                CHARACTERS(a) == CHARACTERS(a+b) == CHARACTERS(a+b+b)):
+                characters(a) == characters(a+b) == characters(a+b+b)):
             print a, a+b, a+b+b
