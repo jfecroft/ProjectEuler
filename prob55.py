93db03d867596bc965ed13f1bc5f08388a249a64
jfecroft <j.f.e.croft@gmail.com>
2015-09-13 11:51:37 +0100

    [cl]

--- prob55.py
+++ prob55.py
@@ -1,18 +1,30 @@
+"""
+sove project euler problem 55
+"""
 from utils import is_palindrome
+import sys
+
 
 def lychrel_number(num, max_rec=50, rec=0):
+    """
+    return if is lychrel_number
+    """
     if is_palindrome(num):
         return True
     elif rec >= max_rec:
         return False
     else:
-        return lychrel_number(num + int(str(num)[::-1]), rec=rec+1) 
+        return lychrel_number(num + int(str(num)[::-1]), rec=rec+1)
+
 
 def main():
+    """
+    execute from command line
+    """
     total = 0
     for i in range(10000):
         total += not lychrel_number(i)
     print total
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
