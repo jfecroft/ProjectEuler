f14f0817972d19281e5ffc9ffee8e0b21417de0c
jfecroft <j.f.e.croft@gmail.com>
2017-08-05 18:24:05 -0700

    slow verson just refamilierilzing

---
+++ prob3_.hs
@@ -0,0 +1,7 @@
+num = 13195
+
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+is_prime n = if factors n == [1,n] then True else False
+
+largest_prime_factor n = maximum [x | x <- (factors n), is_prime x]
