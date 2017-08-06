33dbf0d118a1258fac7c041d0f36cf40addad480
jfecroft <j.f.e.croft@gmail.com>
2017-08-05 18:25:36 -0700

    working version

--- prob3_.hs
+++ prob3_.hs
@@ -1,3 +1,5 @@
+main :: IO ()
+main = print problem_3
 num = 13195
 
 factors n = [x | x <- [1..n], n `mod` x == 0]
@@ -5,3 +7,4 @@ factors n = [x | x <- [1..n], n `mod` x == 0]
 is_prime n = if factors n == [1,n] then True else False
 
 largest_prime_factor n = maximum [x | x <- (factors n), is_prime x]
+problem_3 = largest_prime_factor num
