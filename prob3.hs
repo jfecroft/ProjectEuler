5601371bfb5354451d4a2fafe6cdd59969ca52f8
jfecroft <j.f.e.croft@gmail.com>
2017-08-10 15:06:38 -0700

    tidy up solution

--- prob3.hs
+++ prob3.hs
@@ -1,21 +1,15 @@
-import Data.List
-import Data.Maybe
-
 main :: IO ()
 main = print problem_3
-num = 13195
+num = 600851475143
 
 factors n = [x | x <- [2..n], n `mod` x == 0]
 
-is_prime n = if factors n == [1,n] then True else False
 smallest_divisor n = head (factors n)
-
-largest_prime_factor n = maximum [x | x <- (factors n), is_prime x]
-problem_3 = largest_prime_factor num
-
 
 prime_factors n
   |  n `div` x ==1 = x:[]
   | otherwise = x: prime_factors (n `div` x)
   where
     x = smallest_divisor n
+
+problem_3 = last (prime_factors num)
